4efceda524aa27a54863758d1a0b21bd9bc357d2
Rajat Monga <rajatmonga29@gmail.com>
2018-07-21 17:08:42 -0500

    Update MainActivity.kt

--- Kotlin Files/MainActivity.kt
+++ Kotlin Files/MainActivity.kt
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.example.rajatmonga.kotlinchatapp.R.id.email_edit_text_register
+import com.example.rajatmonga.kotlinchatapp.R.id.password_edit_text_register
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,16 +18,61 @@ class LoginActivity : AppCompatActivity() {
 
         register_button_register.setOnClickListener {
 
-            performRegister()
+            val email = email_edit_text_register.text.toString()
+            val password = password_edit_text_register.text.toString()
 
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText( this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+
+
+            }
+
+            Log.d( "MainActivity", "email is " + email)
+            Log.d( "MainActivity", "password is  $password")
+
+            //Firebase authentication with email, username, and password
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword("9199999989@pintech.com", "corrfecthorsebatterystaple")
+                    .addOnCompleteListener {
+                        if(!it.isSuccessful) return@addOnCompleteListener
+
+                        //else if successful
+
+                        Log.d( "Main", "createdUser with uid: ${it.result.user.uid}")
+
+                    }.addOnFailureListener {
+                Log.d( "Main", "Failed to create User: ${it.message}")
+                Toast.makeText( this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+            }
+
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+
+
+                    .addOnCompleteListener {
+                        if(!it.isSuccessful) return@addOnCompleteListener
+
+                        //else if successful
+
+                        Log.d( "Main", "createdUser with uid: ${it.result.user.uid}")
+
+                    }.addOnFailureListener {
+                        Log.d( "Main", "Failed to create User: ${it.message}")
+                        Toast.makeText( this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+                    }
+
+
+            Log.d("MainActivity", "after create")
         }
+
+
+
+
 
         already_have_account_text_view.setOnClickListener {
             Log.d("MainActivity", "try to show login activity")
 
             // launch the login activity
             val intent = Intent(this, LoginActivity::class.java)
-
             startActivity(intent)
         }
 
@@ -50,6 +97,7 @@ class LoginActivity : AppCompatActivity() {
         //Firebase authentication with email, username, and password
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+
                 .addOnCompleteListener {
                     if(!it.isSuccessful) return@addOnCompleteListener
 
