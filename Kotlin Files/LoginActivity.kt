e5008965cd74c9b3f1e33fa1d22fc21adcbe2a82
Rajat Monga <rajatmonga29@gmail.com>
2018-07-21 17:23:06 -0500

    Update LoginActivity.kt

--- Kotlin Files/LoginActivity.kt
+++ Kotlin Files/LoginActivity.kt
@@ -2,32 +2,41 @@ package com.example.rajatmonga.kotlinchatapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activitiy_login.*
 
 /**
  * Created by rajatmonga on 7/14/18.
  */
 
 class LoginActivity: AppCompatActivity() {
-    
-    super.onCreate(savedInstanceState)
-    
-    setContentView(R.layout.activity_login)
-    
-    login_button_login.setOnClickListener {
-        val email = email_edittext_login.text.toString()
-        val password = password_edittext_login.text.toString()
-        
-        Log.d ( "Login", "Attempt to login with email & pw: $email")
-        
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-            //.addOnCompleteListener()
-            
-        
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activitiy_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d ( "Login", "Attempt to login with email & pw: $email")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            //.addOnCompleteListener()
+
+
+        }
+
+
+        
     }
 
+
+
+    override fun setContentView(view: View?) {
+        super.setContentView(R.layout.activitiy_login)
+    }
 }
+
